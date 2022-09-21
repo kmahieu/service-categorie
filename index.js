ad011e117cbfd44fb634ede8a316bec3e06abde0
Kevin Mahieu <82836519+kevdonche@users.noreply.github.com>
2022-09-21 16:18:30 +0200

    test jenkins

--- index.js
+++ index.js
@@ -33,4 +33,6 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
         .catch((error) => console.log(error.message));
 
+
+        // hhhh
 export default app;
