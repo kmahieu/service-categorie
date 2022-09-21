4cace4e78edd15b10f284175b7e85d2ba4323b30
Kevin Mahieu <82836519+kevdonche@users.noreply.github.com>
2022-09-21 16:34:01 +0200

    test jenkins

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: tr
         .catch((error) => console.log(error.message));
 
 
-        // hhhhddddddsssdzadzasss
+        // hhhhddddddsssdzadzasssddd
 export default app;
